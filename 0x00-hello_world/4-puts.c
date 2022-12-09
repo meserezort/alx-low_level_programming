2d12113ea7383e50d697a9820e61200f08219bd0
meserezort <meselenzba@gmail.com>
2022-12-09 10:49:06 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
-/*
- *
- *  * main -print a line of code using puts
- *
- *   * 
- *
- *    * Return: 0
- *
- *    */
+/**
+ * main - main block
+ *return: 0
+ */
 
 int main(void)
 
 {
-       	puts("\"programming is like building a multilingual puzzle");
+
+	puts("\"Programming is like deveop a multilingual puzzle");
+
 	return (0);
 
 }
+
+
