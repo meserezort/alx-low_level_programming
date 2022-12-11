9b43dc1309ce7d41f9934f8ca0bfafda0f0b6ce4
meserezort <meselenzba@gmail.com>
2022-12-11 14:31:02 -0800

    101-quote.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a new line, to standard error.
@@ -10,6 +11,7 @@ int main(void)
 	write(2,
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 	       59);
+
 	return (1);
 }
 
