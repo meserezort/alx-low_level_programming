879716fec402475dc16893b7c497b33ef8c2d419
meserezort <meselenzba@gmail.com>
2022-12-11 14:12:34 -0800

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - Entery ponit
+ * main - Prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a new line.
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 			return (0);
 }
