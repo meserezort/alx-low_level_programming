74c588a2a1b2d6a3f91d6252941008ca0fbb480d
meserezort <meselenzba@gmail.com>
2022-12-11 14:08:05 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - main block
- * return: 0
+ * main - Prints "Programming is like building a multilingual
+ * puzzle, followed by a new line.
+ *
+ * Return: Always 0 (success)
  */
-
 int main(void)
-
 {
+		puts("\"Programming is like building a multilingual puzzle");
 
-	puts ("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+			return (0);
 }
-
-
