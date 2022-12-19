d866b54a43a0b540a131c94b0731a5565362f788
meserezort <meselenzba@gmail.com>
2022-12-19 09:44:52 -0800

    task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,17 @@
  */
 void print_rev(char *s)
 {
-	int index;
-	
-	for (index =0; s[index]  != ‘ \0 ’; index++)
-	for (index = index-1; s[index] != ‘ \0 ’ ; index--)
+	int fcounter = 0;
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		_putchar(s[index]);
+		fcounter++;
 	}
-	_putchar(‘\n’);
+
+	for (n = (fcounter - 1); n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
 }
