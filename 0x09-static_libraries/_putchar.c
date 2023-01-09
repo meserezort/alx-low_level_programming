fe96f4121dc69de3674ce9ff3e95903d7e9fe049
meserezort <meselenzba@gmail.com>
2023-01-09 12:24:22 -0800

    task0

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,1 +1,12 @@
+#include <unistd.h>
 
+/**
+ * _putchar - writes the character c to stdout
+ * _putchar - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+{
+	return (write(1, &c, 1));
+}
