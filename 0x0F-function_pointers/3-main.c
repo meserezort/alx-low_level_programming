70cd4a59f30745cd7003f17eb90d7aa033ae79a7
meserezort <meselenzba@gmail.com>
2023-01-18 10:28:22 -0800

    taskall

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,7 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - get_op_func has operators correlated with
  * func signs and funcs from op_func
@@ -14,35 +15,26 @@
  */
 int main(int argc, char *argv[])
 {
-	int one, two, ans;
-	int (*res)(int, int);
-	char *get_op;
+	int num1, num2, result;
+	int (*skrt)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	
+	num1 = atoi(argv[1]),
+	num2 = atoi(argv[3]);
 
-	one = atoi(argv[1]);
-	two = atoi(argv[3]);
-	get_op = argv[2];
-
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	skrt = get_op_func(argv[2]);
+	if (skrt == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	result = skrt(num1, num2);
+	printf("%d\n", result);
 
-	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	res = get_op_func(get_op);
-	ans = res(one, two);
-
-	printf("%d\n", ans);
 	return (0);
 }
