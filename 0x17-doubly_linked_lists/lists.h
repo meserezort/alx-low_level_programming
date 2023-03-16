54192a228cb5ac6d8b407c54884423f297c4bd2b
meserezort <meselenzba@gmail.com>
2023-03-16 03:20:59 -0700

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,7 +1,7 @@
-#ifndef _LISTSH_
-#define _LISTH_
-
+#ifndef LISTS
+#define LISTS
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 /**
@@ -19,7 +19,6 @@ typedef struct dlistint_s
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
-
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
